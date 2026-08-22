d90ce084adac6e322b2e51ebe06cf6fb99bdfd7a
Tirth Rathod <tirthrathod05@gmail.com>
2026-08-22 13:07:00 +0530

    Getting env variables

---
+++ backend/config.py
@@ -0,0 +1,11 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Settings:
+    DATABASE_URL: str = os.getenv("DATABASE_URL", "postgresql://postgres:postgres@localhost:5432/globetrotter")
+    # SECRET_KEY: str = os.getenv("SECRET_KEY", "fallback_secret_key")
+    # ALGORITHM: str = os.getenv("ALGORITHM", "HS256")
+
+settings = Settings()
